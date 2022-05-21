a1c6e63f913016d22c1f94d52be2ebadd345fa68
srnyx <25808801+srnyx@users.noreply.github.com>
2022-05-21 17:18:42 -0400

    Now listens to leave events in Friends VC
    If there is one person in Friends VC it will move them to the Waiting for friend  VC
    I also made it ignore bots

--- src/main/java/xyz/srnyx/srnyx_bot/Listeners.java
+++ src/main/java/xyz/srnyx/srnyx_bot/Listeners.java
@@ -4,6 +4,7 @@ import net.dv8tion.jda.api.entities.AudioChannel;
 import net.dv8tion.jda.api.entities.Guild;
 import net.dv8tion.jda.api.entities.Member;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceJoinEvent;
+import net.dv8tion.jda.api.events.guild.voice.GuildVoiceLeaveEvent;
 import net.dv8tion.jda.api.events.guild.voice.GuildVoiceMoveEvent;
 import net.dv8tion.jda.api.hooks.ListenerAdapter;
 
@@ -28,16 +29,43 @@ public class Listeners extends ListenerAdapter {
     }
 
     /**
-     * Moves a user to a new voice channel
+     * Called when a user leaves a voice channel
+     */
+    @Override
+    public void onGuildVoiceLeave(GuildVoiceLeaveEvent event) {
+        move(event.getChannelLeft());
+    }
+
+    /**
+     * Moves users from either the Waiting for friend VC or the Friends VC to the other one
      *
-     * @param   channel The channel to move the user to
+     * @param   channel Channel they joined/left
      */
     private void move(AudioChannel channel) {
-        final List<Member> members = channel.getMembers();
-        final Guild guild = channel.getGuild();
+        new java.util.Timer().schedule(new java.util.TimerTask() {
+            public void run() {
+                final List<Member> members = channel.getMembers();
+                final Guild guild = channel.getGuild();
 
-        if (channel.getId().equals("977313982320873503") && members.size() >= 2) {
-            for (final Member member : members) guild.moveVoiceMember(member, guild.getVoiceChannelById("960999744753250315")).queue();
-        }
+                int size = 0;
+                for (final Member member : members) if (!member.getUser().isBot()) size++;
+
+                boolean sizeCheck = false;
+                String moveTo = "";
+                if (channel.getId().equals("960999744753250315")) {
+                    sizeCheck = size < 2;
+                    moveTo = "977313982320873503";
+                }
+                if (channel.getId().equals("977313982320873503")) {
+                    sizeCheck = size >= 2;
+                    moveTo = "960999744753250315";
+                }
+
+                if (sizeCheck) for (final Member member : members) guild.moveVoiceMember(member, guild.getVoiceChannelById(moveTo)).queue();
+            }
+        }, 1000);
     }
+
+    // 960999744753250315 Friends VC
+    // 977313982320873503 Waiting for friend VC
 }
