54c658eafb0ac149b42c000e694568174984d2ca
srnyx <25808801+srnyx@users.noreply.github.com>
2025-06-05 16:06:02 -0400

    Update dependencies
    Lazy Library: e7af7ab598 -> 5e06984a32
    JDA: 5.3.2 -> 5.5.1

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,6 @@ plugins {
     id("com.gradleup.shadow") version "8.3.6"
 }
 
-setupLazyLibrary("e7af7ab598", "5.3.2", "xyz.srnyx", "2.0.0")
+setupLazyLibrary("5e06984a32", "5.5.1", "xyz.srnyx", "2.0.0")
 
 dependencies.compileOnly("io.github.freya022", "BotCommands", "2.10.4") // For documentation
