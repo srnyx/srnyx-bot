c429ecc44acd495c6808f1de376dde15e7530d20
srnyx <25808801+srnyx@users.noreply.github.com>
2025-06-05 16:16:27 -0400

    Update Java Utilities and enable `HttpUtility` debug

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,6 @@ plugins {
     id("com.gradleup.shadow") version "8.3.6"
 }
 
-setupLazyLibrary("5e06984a32", "5.5.1", "xyz.srnyx", "2.0.0")
+setupLazyLibrary("bdda70fd3d", "5.5.1", "xyz.srnyx", "2.0.0")
 
 dependencies.compileOnly("io.github.freya022", "BotCommands", "2.10.4") // For documentation
