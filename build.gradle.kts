d520169890dbbf7e5129da40d5d876dacfed24df
srnyx <25808801+srnyx@users.noreply.github.com>
2025-07-21 19:22:43 -0400

    Update Gradle and Shadow
    Gradle: 8.14.2 -> 8.14.3
    Shadow: 8.3.7 -> 8.3.8

--- build.gradle.kts
+++ build.gradle.kts
@@ -4,7 +4,7 @@ import xyz.srnyx.gradlegalaxy.utility.setupLazyLibrary
 plugins {
     application
     id("xyz.srnyx.gradle-galaxy") version "1.3.3"
-    id("com.gradleup.shadow") version "8.3.7"
+    id("com.gradleup.shadow") version "8.3.8"
 }
 
 setupLazyLibrary("976f3062f3", "5.6.1", "xyz.srnyx", "2.0.0")
