c2a1204e0dc3784f9435e91ce4ad73673d094fd4
srnyx <25808801+srnyx@users.noreply.github.com>
2025-08-12 20:12:56 -0400

    Update dependencies
    Gradle: 8.14.3 -> 9.0.0
    Lazy Library: 976f3062f3 -> c875725746

--- build.gradle.kts
+++ build.gradle.kts
@@ -7,6 +7,6 @@ plugins {
     id("com.gradleup.shadow") version "8.3.8"
 }
 
-setupLazyLibrary("976f3062f3", "5.6.1", "xyz.srnyx", "2.0.0")
+setupLazyLibrary("c875725746", "5.6.1", "xyz.srnyx", "2.0.0")
 
 dependencies.compileOnly("io.github.freya022", "BotCommands", "2.10.4") // For documentation
