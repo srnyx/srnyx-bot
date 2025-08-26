40ecd27d57a707b2e54b958697c344c8840c9352
srnyx <25808801+srnyx@users.noreply.github.com>
2025-08-26 17:19:16 -0400

    Update Gradle Galaxy
    1.3.3 -> 1.3.4

--- build.gradle.kts
+++ build.gradle.kts
@@ -1,24 +1,11 @@
-import xyz.srnyx.gradlegalaxy.utility.lazyLibrary
-import xyz.srnyx.gradlegalaxy.utility.setupJda
+import xyz.srnyx.gradlegalaxy.utility.setupLazyLibrary
 
 
 plugins {
     application
-    id("xyz.srnyx.gradle-galaxy") version "1.3.3"
+    id("xyz.srnyx.gradle-galaxy") version "1.3.4"
     id("com.gradleup.shadow") version "8.3.8"
     id("dev.reformator.stacktracedecoroutinator") version "2.5.6"
 }
 
-lazyLibrary("botcommands-v3-SNAPSHOT")
-setupJda("5.6.1", "xyz.srnyx", "2.0.0", "General purpose bot for srnyx")
-
-//TODO temporary
-configurations.all {
-    resolutionStrategy.cacheDynamicVersionsFor(0, "seconds")
-}
-
-// Fix some tasks
-tasks["distZip"].dependsOn("shadowJar")
-tasks["distTar"].dependsOn("shadowJar")
-tasks["startScripts"].dependsOn("shadowJar")
-tasks["startShadowScripts"].dependsOn("jar")
+setupLazyLibrary("botcommands-v3-SNAPSHOT", "5.6.1", "xyz.srnyx", "2.0.0", "General purpose bot for srnyx")
