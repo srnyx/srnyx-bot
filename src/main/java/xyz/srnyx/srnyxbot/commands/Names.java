3512884fc6f3f2b32d3ab7599f2282c9fb0006dd
srnyx <25808801+srnyx@users.noreply.github.com>
2026-05-03 22:15:57 -0400

    Add `/names` command

---
+++ src/main/java/xyz/srnyx/srnyxbot/commands/Names.java
@@ -0,0 +1,40 @@
+package xyz.srnyx.srnyxbot.commands;
+
+import io.github.freya022.botcommands.api.commands.annotations.Command;
+import io.github.freya022.botcommands.api.commands.annotations.UserPermissions;
+import io.github.freya022.botcommands.api.commands.application.slash.GuildSlashEvent;
+import io.github.freya022.botcommands.api.commands.application.slash.annotations.JDASlashCommand;
+import io.github.freya022.botcommands.api.commands.application.slash.annotations.SlashOption;
+
+import net.dv8tion.jda.api.Permission;
+import net.dv8tion.jda.api.entities.Member;
+
+import org.jetbrains.annotations.NotNull;
+
+import xyz.srnyx.lazylibrary.emoji.LazyEmoji;
+
+
+@Command
+public class Names {
+    @UserPermissions({Permission.NICKNAME_MANAGE, Permission.MANAGE_SERVER})
+    @JDASlashCommand(
+            name = "names",
+            description = "Get all members with a specific name")
+    public void names(@NotNull GuildSlashEvent event,
+                      @SlashOption(description = "The substring to check for") @NotNull String substring) {
+        event.deferReply(true).queue();
+        event.getGuild().loadMembers()
+                .onSuccess(members -> {
+                    // Get members
+                    final StringBuilder builder = new StringBuilder();
+                    for (final Member member : members) if (member.getEffectiveName().contains(substring)) builder.append(member.getAsMention()).append("\n");
+                    if (builder.isEmpty()) {
+                        event.getHook().editOriginal(LazyEmoji.NO + " No members with that substring in their name!").queue();
+                        return;
+                    }
+
+                    // Reply
+                    event.getHook().editOriginal(builder.toString()).queue();
+                });
+    }
+}
